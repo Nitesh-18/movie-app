6a27375ba78c7289573c812afd458af109a86d4a
Nitesh <niteshranjankar5@gmail.com>
2024-08-06 01:07:48 +0530

    "Updated .gitignore, package.json, and App.js files"

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import "./App.css";
 import SearchIcon from "./search.svg";
 import MovieCard from "./MovieCard";
